01fa980824467438d1fefea8e424bff4740ca322
Nikita <61729611+KotikNikita@users.noreply.github.com>
2021-10-26 20:22:55 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Start downloading"
 ls -la
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1o9pyskkKMxuomI5BWuLjCG2nSv5iePZz' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1o9pyskkKMxuomI5BWuLjCG2nSv5iePZz" -O data.zip && rm -rf /tmp/cookies.txt
+sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1o9pyskkKMxuomI5BWuLjCG2nSv5iePZz' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1o9pyskkKMxuomI5BWuLjCG2nSv5iePZz" -O data.zip && rm -rf /tmp/cookies.txt
 ls -la
 chmod 777 ./data.zip
 
-unzip data.zip
+sudo unzip data.zip
 chmod ./ModelNet40_MeshNet
 rm -rf data.zip
 rm -rf ./ModelNet40_MeshNet/.DS_Store
