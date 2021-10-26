8fd2419f7ea2b653efc0c7434e85ee225c7833ea
Nikita <61729611+KotikNikita@users.noreply.github.com>
2021-10-26 20:29:34 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,11 +3,11 @@ echo "Start downloading"
 ls -la
 sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1o9pyskkKMxuomI5BWuLjCG2nSv5iePZz' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1o9pyskkKMxuomI5BWuLjCG2nSv5iePZz" -O data.zip && rm -rf /tmp/cookies.txt
 ls -la
-chmod 777 ./data.zip
+sudo chmod 777 ./data.zip
 
 sudo unzip data.zip
-chmod ./ModelNet40_MeshNet
-rm -rf data.zip
-rm -rf ./ModelNet40_MeshNet/.DS_Store
+sudo chmod ./ModelNet40_MeshNet
+sudo rm -rf data.zip
+sudo rm -rf ./ModelNet40_MeshNet/.DS_Store
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1l8Ij9BODxcD1goePBskPkBcgKW76Ewcs' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1l8Ij9BODxcD1goePBskPkBcgKW76Ewcs" -O MeshNet_best_9192.pkl && rm -rf /tmp/cookies.txt
+sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1l8Ij9BODxcD1goePBskPkBcgKW76Ewcs' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1l8Ij9BODxcD1goePBskPkBcgKW76Ewcs" -O MeshNet_best_9192.pkl && rm -rf /tmp/cookies.txt
